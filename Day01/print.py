449a252cde675805e2325b616d2291f26998d475
johnwick020213 <ganbo020213@gmail.com>
2024-07-22 15:42:10 +0800

    注释，多行打印

---
+++ Day01/print.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+
+word='word'
+line="this is a line"
+paragraph="""this is a paragraph,
+sdfkjdslfjdsjflds
+fdsfdsfdsfdsfdsfdsf"""
+
+#注释
+print(word)
+print(line)
+print(paragraph)
